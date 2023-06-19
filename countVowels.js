252fdd0795562c8b45d2b8d785014421a5beecf7
Tarun Sharma <Sharmaforusre@gmail.com>
2023-06-19 20:33:42 +0530

    DSA Problems

---
+++ countVowels.js
@@ -0,0 +1,17 @@
+const prompt= require('prompt-sync')()
+
+function countVowel(str) { 
+
+    // find the count of vowels
+    const count = str.match(/[aeiou]/gi).length;
+
+    // return number of vowels
+    return count;
+}
+
+// take input
+const string = prompt('Enter a string: ');
+
+const result = countVowel(string);
+
+console.log(result);
